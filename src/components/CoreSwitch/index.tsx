29b7cfc0da74dd959c5ab2546b726e1bb1a07804
TinhNguyenTB <tinh271103@gmail.com>
2025-12-10 10:04:36 +0700

    feat: CoreSwitch component

---
+++ src/components/CoreSwitch/index.tsx
@@ -0,0 +1,49 @@
+import { Controller, Control, FieldValues, Path, RegisterOptions } from 'react-hook-form'
+import { Form, Switch } from 'antd'
+
+interface CoreSwitchProps<T extends FieldValues> {
+  name: Path<T>
+  control: Control<T>
+  label?: string
+  disabled?: boolean
+  required?: boolean
+  rules?: RegisterOptions<T, Path<T>>
+}
+
+export function CoreSwitch<T extends FieldValues>({
+  name,
+  control,
+  label,
+  disabled = false,
+  required = false,
+  rules
+}: CoreSwitchProps<T>) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field, fieldState: { error } }) => (
+        <Form.Item
+          label={
+            required && label ? (
+              <span>
+                {label} <span style={{ color: 'red' }}>*</span>
+              </span>
+            ) : (
+              label
+            )
+          }
+          validateStatus={error ? 'error' : ''}
+          help={error?.message}
+        >
+          <Switch
+            checked={field.value}
+            onChange={(val) => field.onChange(val)}
+            disabled={disabled}
+          />
+        </Form.Item>
+      )}
+    />
+  )
+}
